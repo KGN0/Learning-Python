bff7e552283f0007b3b769c9ad0b3ef18832bc0b
K G N <kgn.00004@gmail.com>
2026-06-01 22:41:50 +0530

    type conversion is commited

---
+++ Python Basics/6_type_conversion.py
@@ -0,0 +1,41 @@
+# Type conversion
+'''
+This process of converting values for one type to another type is known as type conversion
+Types of type conversion
+1. Implicit type conversion = automatically performed by the python interpreter without programmer;s intervention
+2. Explicit type conversion = this conversion is user defined
+'''
+# example for implicit type conversion
+a = True
+print(type(a))
+
+b = 5
+print(type(b))
+
+c = a + b
+print(type(c))
+
+# example 2
+a = 15
+print(type(a))
+
+b = 17.5
+print(type(b))
+
+c = a + b
+print(type(5))
+
+# examples for explicit type conversion
+m = float(35)
+print(type(m))
+
+o = float('7')
+print(type(o))
+
+# type conversion functions
+'''
+- int()
+- str()
+- float()
+- bool()
+'''
